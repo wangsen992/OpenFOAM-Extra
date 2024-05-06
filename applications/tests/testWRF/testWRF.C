1a01555ff9b198b1f28eea495ceae227b3ec6de6
Sen Wang <wangsen992@gmail.com>
2024-05-06 23:57:49 +0000

    added args to control test portion

--- applications/tests/testWRF/testWRF.C
+++ applications/tests/testWRF/testWRF.C
@@ -7,6 +7,7 @@ using namespace std;
 using namespace Foam;
 int main(int argc, char **argv)
 {
+  argList::addOption("test", "test name", "test only getVar");
   // Load openfoam case
   std::string case_name("foamcase/system/controlDict"); 
 
@@ -33,31 +34,37 @@ int main(int argc, char **argv)
   WRF wrf("ncfile", "./", "wrfCase", runTime, proj_string);
 
   // Testing get var function
+  if(args.options()["test"] == Foam::string("getVar"))
   {
-    Info<< average(wrf.U(1)) << endl;
-    Info<< average(wrf.U(2)) << endl;
-    Info<< average(wrf.U(3)) << endl;
-  }
-  {
-    Info<< average(wrf.var("T", 1)) << endl;
-    Info<< average(wrf.var("T", 2)) << endl;
-    Info<< average(wrf.var("T", 3)) << endl;
+    {
+      Info<< average(wrf.U(1)) << endl;
+      Info<< average(wrf.U(2)) << endl;
+      Info<< average(wrf.U(3)) << endl;
+    }
+    {
+      Info<< average(wrf.var("T", 1)) << endl;
+      Info<< average(wrf.var("T", 2)) << endl;
+      Info<< average(wrf.var("T", 3)) << endl;
+    }
   }
 
   // Testing interpolation of close-to-patch cells
-  const pointField& foamCellCentres(mesh.cellCentres());
-  labelList patchCellInd(getPatchCloseCells(mesh, "inlet", 20));
-  scalarField Tinterp(patchCellInd.size());
-  pointField patchCellCentres(patchCellInd.size());
-  Info << "Running std::transform" << endl;
-  std::transform
-  (
-    patchCellInd.cbegin(), 
-    patchCellInd.cend(),
-    patchCellCentres.begin(),
-    [&](label i) { return wrf.transform(foamCellCentres[i]); }
-  );
-  Info << wrf.interpolate<Foam::vector>(patchCellCentres, wrf.U(2).ref()) << endl;;
+  if(args.options()["test"] == Foam::string("interp"))
+  {
+    const pointField& foamCellCentres(mesh.cellCentres());
+    labelList patchCellInd(getPatchCloseCells(mesh, "inlet", 20));
+    scalarField Tinterp(patchCellInd.size());
+    pointField patchCellCentres(patchCellInd.size());
+    Info << "Running std::transform" << endl;
+    std::transform
+    (
+      patchCellInd.cbegin(), 
+      patchCellInd.cend(),
+      patchCellCentres.begin(),
+      [&](label i) { return wrf.transform(foamCellCentres[i]); }
+    );
+    Info << wrf.interpolate<Foam::vector>(patchCellCentres, wrf.U(2).ref()) << endl;;
+  }
 
   return 0;
 }
